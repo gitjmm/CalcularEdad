b4c9629af6d0d4656fc8c7aad257a936c5fe439c
Jorge Martínez <jmmostazo.medac@gmail.com>
2023-07-20 10:40:46 +0200

    Comentarios e idiomas

--- app/src/main/java/com/jmmostazo/calcularedad/MainActivity.java
+++ app/src/main/java/com/jmmostazo/calcularedad/MainActivity.java
@@ -2,14 +2,24 @@ package com.jmmostazo.calcularedad;
 /*
 * Proyecto Jorge Martínez Mostazo
 * App Android que calcula la edad perruna a partir de la humana
+* Elemntos usados en esta app
+* 1. Crear una app desde inicio en Java
+* 2. Uso de Views: TextView, EditText, Button, LinearLayout
+* 3. strings.xml Confiuración de string de idiomas
+* 4. Diseño de views
+* 5. Uso de Logcat
+* 6. Uso y configuración de github
+* 7. Databinding
 * */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Inicio de elementos Views
+        TextView txtEdad = findViewById(R.id.txtEdad);
         EditText edadHumana;
         edadHumana = findViewById(R.id.etEdadHumana);
         TextView edadPerruna = findViewById(R.id.txtEdadPerro);
@@ -25,19 +37,22 @@ public class MainActivity extends AppCompatActivity {
 
 
         //Uso de Log
-        
+        Log.d("MainActivity","Actvity creado");
 
-
+        txtEdad.setText(R.string.dime_edad);
         btnCalcular.setOnClickListener(view -> {
             //Pasamos el numero de edad a un string
             String edad = edadHumana.getText().toString();
 
             int edadInt = 0;
             if (!edad.isEmpty()) edadInt = Integer.parseInt(edad);
-            else edadPerruna.setText("Introduce una edad");
+            else {
+                Log.d("MainActivity","Campo edad vacío");
+                Toast.makeText(this,getString(R.string.introduce_edad),Toast.LENGTH_SHORT).show();
+            }
 
             int resultado = edadInt*7;
-            edadPerruna.setText("Edad Perruna: "+resultado+" años");
+            edadPerruna.setText(getString(R.string.resultado,resultado));
 
         });
 
